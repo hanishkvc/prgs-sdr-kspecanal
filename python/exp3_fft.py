a313d5904cc88dfdd643a8dfdc9bd659d5bb6f6a
HanishKVC <hanishkvc@gmail.com>
2017-02-20 16:22:16 +0530

    exp3_fft.py - dataSamples used for Fft, saving plot
    * The period over which samples are captured (rather generated in this case)
      to be analysed wrt fft, is chosen randomly now. Note that it is the starting
      time which is randomly selected, however the length of the period is fixed
      for now.
    
    * Added logic to check if the data samples being considered is a multiple/
      submultiple of the sampling rate. And inturn print the strength of the
      frequencies noticied in that data.
    
      From this it was noticied that not only multiples/submultiples but also
      many sample lengths which form a rational fraction with the sampling rate
      also maintain the relative strengths of the frequencies in the data (i.e
      they don't skew the results one way or the other).
    
      HOWEVER some sample lengths which seem to form a rational number with the
      sampling rate still do show skew in the fft results. Example are
      (10000/1875), (10000/3750). So something more fundamental at play here,
      have to check later. FOR NOW, as long as the sample length used for fft
      is a multiple or submultiple of the sampling rate, the fft results seem
      to be fine.
    
      NEXT HAVE TO vary the sample length over a narrow range and then see how
      much it affects the fft result.
    
      Added a simple logic to check if two given numbers are multiples or
      submultiples of one another, however do note that it is dependent on python
      comparing a integer and its decimal representation has equal properly.
    
      Also to simplify the printing of the fft results, the fft results are rounded
      to a single decimal point digit, as we know before hand that the frequencies
      available in the sample have sufficiently large power, such that this rounding
      doesn't create any unwanted skew.
    
    * Added logic to save the plot into a ps file properly
    
      * Used plt.figure(num=1, figsize) to set the canvas or figure size so that
        a dense subplotted image is properly spread out and not overlapping.
    
      * called plt.savefig to save into ps file. However noticied that DPI didnt
        have any impact for ps file saving (Rather papertype and orientation did
        have impact). Most probably dpi is used for svg and some other formats.

--- python/exp3_fft.py
+++ python/exp3_fft.py
@@ -3,15 +3,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+sr = 1024
 sr = 10e3
-totalTime = 3 #4
-t = np.arange(0+0.137, totalTime+0.137, 1/sr)
+totalTime = 4
+totalTime = 3
+sampleStartTime = np.random.rand(1)
+print("sampleStartTime[{}]".format(sampleStartTime))
+t = np.arange(0+sampleStartTime, totalTime+sampleStartTime, 1/sr)
 
 s10k = np.sin(2*np.pi*1e3*t)
 s20k = np.sin(2*np.pi*2e3*t)
 s17k = np.sin(2*np.pi*1.7e3*t)
 
 s=s10k+s20k+s17k
+
+#DANGER: Note that the comparision of integer with floating point below
+# depends on how the language handles such comparison. New versions of
+# python seem to handle them better.
+def check_multipleOrSubmultiple(mOrS, refNum):
+    if (mOrS >= refNum):
+        ratio = mOrS/refNum
+        ratioInt = int(ratio)
+        backFromInt = ratioInt*refNum
+        backFromDec = ratio*refNum
+        if (ratioInt == ratio):
+            return True, "Multiple"
+        else:
+            return False, "NOT Multiple"
+    if (mOrS < refNum):
+        ratio = refNum/mOrS
+        ratioInt = int(ratio)
+        backFromInt = ratioInt*mOrS
+        backFromDec = ratio*mOrS
+        if (ratioInt == ratio):
+            return True, "SubMultiple"
+        else:
+            return False, "NOT SubMultiple"
+
+
+def check_fftresult(fftD, dataLen, sr):
+    ratio = sr/dataLen
+    fftMin = np.min(fftD)
+    fftMax = np.max(fftD)
+    #fClipped = np.clip(fftD, 0.1, 1.0)
+    fRounded = np.round(fftD, 1)
+    fftUniqRounded = np.unique(fRounded)
+    bMOrS, sType = check_multipleOrSubmultiple(dataLen, sr)
+    print("sampleLen[{}], samplingRate[{}], ratio[{}], min[{}], max[{}]\n ratioType[{}], fftUniqRounded[{}]".format(dataLen, sr, ratio, 
+        fftMin, fftMax, sType, fftUniqRounded))
 
 def do_fft(s, startFreq, endFreq, sr):
     f=np.fft.fft(s)
@@ -21,13 +60,21 @@ def do_fft(s, startFreq, endFreq, sr):
     fr=np.linspace(startFreq, endFreq,len(f))
     plt.plot(fr,f)
     plt.grid()
-    plt.title("sampleLen[{}]".format(len(s)))
+    dataLen = len(s)
+    ratio = sr/dataLen
+    plt.title("sampleLen[{}], ratioWithSR[{}]".format(dataLen, ratio))
+    check_fftresult(f, dataLen, sr)
 
 def exp_fft(s, startFreq, endFreq, sr):
     for i in range(1,21):
         plt.subplot(5,4,i)
         do_fft(s[0:len(s)/i], startFreq, endFreq, sr)
-    plt.show()
+    # dpi option to savefig didn't seem to help in anyway for ps files
+    # Most probably dpi is used for image based formats like svg
+    # while papertype seems to help for ps
+    plt.savefig("/tmp/exp3_fft.ps", papertype="a2", orientation="landscape")
+    #plt.show()
 
+plt.figure(num=1, figsize=(26,20))
 exp_fft(s, 0, sr/2, sr)
 
