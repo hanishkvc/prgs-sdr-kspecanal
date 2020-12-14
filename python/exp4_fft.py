dc1cfe75e938189837d8ec343354c5b3bf4c25bd
hanishkvc <hanishkvc@gmail.com>
2020-12-14 14:54:25 +0530

    PartialCycleWindowedFft: Conv to RectWindow equivalent
    Use the ratio of the rectangle window area and the area of window
    function of interest as the conversion factor for the value got by
    fft of windowed sample to the equivalent value if rectangle window
    had been used.
    
    Add grid to the fft plot.
    
    Also add a 3rd freq to the test sample.

--- python/exp4_fft.py
+++ python/exp4_fft.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 sr = 80
 f1 = 2
 f2 = 8
+f3 = 38
 startTime = 0
 endTime = 10
 
@@ -32,13 +33,15 @@ def plot_fft(fftN):
     '''
     fftN = fftN[:int(len(fftN)/2)]
     freqs = np.linspace(0, sr/2, len(fftN))
+    plt.grid(True)
     plt.plot(freqs, fftN, ".-")
 
 
 # Generate the signal
 s1 = np.sin(2*np.pi*f1*t)
 s2 = np.sin(2*np.pi*f2*t)
-s = s1 + s2
+s3 = np.sin(2*np.pi*f3*t)
+s = s1 + s2 + s3
 print("NumOfSamples:", len(s))
 plt.subplot(nRows, nCols, 1)
 plt.plot(s)
@@ -87,7 +90,8 @@ for i in range(0, 10):
     plt.subplot(nRows, nCols, 7+i*4)
     plt.plot(sT)
     plt.subplot(nRows, nCols, 8+i*4)
-    plot_fft(fftN*2.5)
+    #plot_fft(fftN*2.3)
+    plot_fft(fftN*(len(sT)/win.sum()))
     if winFftMaxMin > max(fftN):
         winFftMaxMin = max(fftN)
     if winFftMaxMax < max(fftN):
