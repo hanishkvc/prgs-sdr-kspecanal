a9acc94d5fe649c67c6c06986198a40ae92a4d92
hanishkvc <hanishkvc@gmail.com>
2020-12-18 10:55:28 +0530

    SpecAnal: ScanRange logic, basic cmdline args

--- python/kspecanal.py
+++ python/kspecanal.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
+# kSpecAnal - A simple spectrum analyser using RtlSdr
+# HanishKVC, v20201218IST1054
+#
 
+
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import rtlsdr
@@ -8,7 +13,7 @@ import rtlsdr
 gNonOverlap = 0.1
 gCenterFreq = 91.7e6
 gSamplingRate = 2.4e6
-gFftSize = 8192
+gFftSize = 2**16
 gFullSize = gFftSize*4
 gGain = 7.1
 
@@ -55,17 +60,43 @@ def zero_span(sdr, d):
         plt.pause(0.001)
 
 
+def scan_range(sdr, d):
+    freqSpan = d['samplingRate']
+    curFreq = d['startFreq'] + freqSpan/2
+    dataFAll = np.array([])
+    freqsAll = np.array([])
+    while curFreq < d['endFreq']:
+        sdr_setup(sdr, curFreq, d['samplingRate'], d['gain'])
+        freqs = np.fft.fftfreq(gFftSize,1/d['samplingRate']) + curFreq
+        dataF = sdr_curscan(sdr)
+        dataFAll = np.append(dataFAll, dataF)
+        freqsAll = np.append(freqsAll, freqs)
+        plt.cla()
+        plt.plot(freqsAll, dataFAll)
+        plt.pause(0.001)
+        curFreq += freqSpan
+
+
 gD = {}
-gD['centerFreq'] = gCenterFreq
 gD['samplingRate'] = gSamplingRate
 gD['gain'] = gGain
-gD['startFreq'] = gD['centerFreq'] - gD['samplingRate']/2
-gD['endFreq'] = gD['centerFreq'] + gD['samplingRate']/2
+
+curMode = sys.argv[1].upper()
+if curMode == 'SCAN':
+    gD['startFreq'] = float(sys.argv[2])
+    gD['endFreq'] = float(sys.argv[3])
+    gD['centerFreq'] = gD['startFreq'] + ((gD['endFreq'] - gD['startFreq'])/2)
+else:
+    gD['centerFreq'] = float(sys.argv[2])
+    gD['startFreq'] = gD['centerFreq'] - gD['samplingRate']/2
+    gD['endFreq'] = gD['centerFreq'] + gD['samplingRate']/2
 print("INFO: startFreq[{}] centerFreq[{}] endFreq[{}], samplingRate[{}]".format(gD['startFreq'], gD['centerFreq'], gD['endFreq'], gD['samplingRate']))
 
 sdr = rtlsdr.RtlSdr()
-#plt.interactive(True)
-zero_span(sdr, gD)
+if curMode == 'SCAN':
+    scan_range(sdr, gD)
+else:
+    zero_span(sdr, gD)
 sdr.close()
 
-
+input("Press any key to quit...")
