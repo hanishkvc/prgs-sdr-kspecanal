d58b4dff9fe3a942261da068e63104d68df1b19d
hanishkvc <hanishkvc@gmail.com>
2020-12-18 10:44:33 +0530

    SpecAnal: Reflow/ReStructure for reuse

--- python/kspecanal.py
+++ python/kspecanal.py
@@ -13,7 +13,7 @@ gFullSize = gFftSize*4
 gGain = 7.1
 
 
-def setup_sdr(sdr, fC, fS, gain):
+def sdr_setup(sdr, fC, fS, gain):
     '''
     Setup rtlsdr.
     Also skip few samples to avoid any junk, when it is settling.
@@ -25,24 +25,30 @@ def setup_sdr(sdr, fC, fS, gain):
     print("SetupSDR: fC[{}] fS[{}] gain[{}]".format(fC, fS, gain))
 
 
+def sdr_curscan(sdr):
+    numLoops = int(gFullSize/(gFftSize*gNonOverlap))
+    print("curscan: numLoops[{}] fullSize[{}]".format(numLoops, gFullSize))
+    samples = sdr.read_samples(gFullSize)
+    fftAll = np.zeros(gFftSize)
+    for i in range(numLoops):
+        iStart = int(i*gFftSize*gNonOverlap)
+        iEnd = iStart + gFftSize
+        tSamples = samples[iStart:iEnd]
+        if len(tSamples) < gFftSize:
+            break
+        fftN = 2*abs(np.fft.fft(tSamples))/len(tSamples)
+        fftAll = (fftAll + fftN)/2
+    fftAll[0] = 0
+    return fftAll
+
 
 def zero_span(sdr, d):
+    sdr_setup(sdr, d['centerFreq'], d['samplingRate'], d['gain'])
     freqs = np.fft.fftfreq(gFftSize,1/d['samplingRate']) + d['centerFreq']
-    numLoops = int(gFullSize/(gFftSize*gNonOverlap))
-    print("ZeroSpan: min[{}] max[{}] numLoops[{}] fullSize[{}]".format(min(freqs), max(freqs), numLoops, gFullSize))
+    print("ZeroSpan: min[{}] max[{}]".format(min(freqs), max(freqs)))
     while True:
         #print(".")
-        samples = sdr.read_samples(gFullSize)
-        fftAll = np.zeros(gFftSize)
-        for i in range(numLoops):
-            iStart = int(i*gFftSize*gNonOverlap)
-            iEnd = iStart + gFftSize
-            tSamples = samples[iStart:iEnd]
-            if len(tSamples) < gFftSize:
-                break
-            fftN = 2*abs(np.fft.fft(tSamples))/len(tSamples)
-            fftAll = (fftAll + fftN)/2
-        fftAll[0] = 0
+        fftAll = sdr_curscan(sdr)
         plt.cla()
         plt.plot(freqs, fftAll)
         plt.show(block=False)
@@ -58,7 +64,6 @@ gD['endFreq'] = gD['centerFreq'] + gD['samplingRate']/2
 print("INFO: startFreq[{}] centerFreq[{}] endFreq[{}], samplingRate[{}]".format(gD['startFreq'], gD['centerFreq'], gD['endFreq'], gD['samplingRate']))
 
 sdr = rtlsdr.RtlSdr()
-setup_sdr(sdr, gD['centerFreq'], gD['samplingRate'], gD['gain'])
 #plt.interactive(True)
 zero_span(sdr, gD)
 sdr.close()
