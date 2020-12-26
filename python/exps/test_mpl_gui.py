619dfa0679aae9367c4c564e993b573821dfe4c1
hanishkvc <hanishkvc@gmail.com>
2020-12-26 14:07:32 +0530

    TestMPLGUI: show or pause is allowing btn triggers to occur

--- python/exps/test_mpl_gui.py
+++ python/exps/test_mpl_gui.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
+import time
 
 def handle_clicked(event):
     print("Btn clicked...")
 
+plt.ion()
 f1 = plt.figure(figsize=(6,4))
 gs = f1.add_gridspec(2,2)
 ax = f1.add_subplot(gs[0,0])
 btn = plt.Button(ax, "test")
 btn.on_clicked(handle_clicked)
-plt.show()
+#plt.show()
+
+while True:
+    for i in range(5):
+        time.sleep(1)
+    plt.pause(0.001)
 
