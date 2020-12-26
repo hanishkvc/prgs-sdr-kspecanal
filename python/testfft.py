0a860ab2738f4dbf05c470c9d616b489e4fed3ae
hanishkvc <hanishkvc@gmail.com>
2020-12-26 21:09:06 +0530

    TestFFT: Fix oversight wrt Freq generation
    Oops I was wrongly trying to generate the specified center and adjacent
    frequencies in absolute terms, instead of relative to current band and
    the sampling rate. This has been fixed now.

--- python/testfft.py
+++ python/testfft.py
@@ -23,15 +23,15 @@ class RtlSdr():
         Actual rtlsdr returns iq data in complex notation.
         This currently returns real data
         '''
-        f1 = self.center_freq - self.sample_rate/4
-        f2 = self.center_freq
-        f3 = self.center_freq + self.sample_rate/4
+        f1 = self.sample_rate/4
+        f2 = 1
+        f3 = -self.sample_rate/4
         dur = size/self.sample_rate
         tStart = random.random()
         tTimes = np.linspace(tStart, tStart+dur, self.sample_rate*dur)
         sT = []
-        f = [ f1, f2, f3 ]
-        print("INFO:testfft_rtlsdr: freqs [{}], sampRate [{}], dur [{}], len [{}]".format(f, self.sample_rate, dur, len(tTimes)))
+        f = np.array([ f1, f2, f3 ])
+        print("INFO:testfft_rtlsdr: freqs [{}], sampRate [{}], dur [{}], len [{}]".format(self.center_freq - f, self.sample_rate, dur, len(tTimes)))
         s = np.zeros(len(tTimes), dtype=complex)
         for i in range(len(f)):
             sS = np.sin(2*np.pi*f[i]*tTimes)
