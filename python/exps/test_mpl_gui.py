76536ea4f16e1f078ff3a1470a646e68131f373a
hanishkvc <hanishkvc@gmail.com>
2020-12-26 14:15:02 +0530

    TestMPLGUI: Btn going out of runtime context
    Rather if the Btn is created in a function and not stored into a
    global context, then when one returns from the function, as the
    button is no longer available outside the function, as it will get
    cleared automatically, so btn doesnt work.

--- python/exps/test_mpl_gui.py
+++ python/exps/test_mpl_gui.py
@@ -6,16 +6,16 @@ import time
 def handle_clicked(event):
     print("Btn clicked...")
 
-plt.ion()
-f1 = plt.figure(figsize=(6,4))
-gs = f1.add_gridspec(2,2)
-ax = f1.add_subplot(gs[0,0])
-btn = plt.Button(ax, "test")
-btn.on_clicked(handle_clicked)
-#plt.show()
+def test_ui():
+    #plt.ion()
+    f1 = plt.figure(figsize=(6,4))
+    gs = f1.add_gridspec(2,2)
+    ax = f1.add_subplot(gs[0,0])
+    btn = plt.Button(ax, "test")
+    btn.on_clicked(handle_clicked)
+    #plt.show()
 
-while True:
-    for i in range(5):
-        time.sleep(1)
-    plt.pause(0.001)
+test_ui()
+plt.show()
+
 
