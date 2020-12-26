160c3ea3579248caf5bd7273ec8f82fca074e629
hanishkvc <hanishkvc@gmail.com>
2020-12-26 14:03:39 +0530

    TestMPLGUI: Added event handler for button
    This is working

--- python/exps/test_mpl_gui.py
+++ python/exps/test_mpl_gui.py
@@ -2,9 +2,13 @@
 
 import matplotlib.pyplot as plt
 
+def handle_clicked(event):
+    print("Btn clicked...")
+
 f1 = plt.figure(figsize=(6,4))
 gs = f1.add_gridspec(2,2)
 ax = f1.add_subplot(gs[0,0])
 btn = plt.Button(ax, "test")
+btn.on_clicked(handle_clicked)
 plt.show()
 
